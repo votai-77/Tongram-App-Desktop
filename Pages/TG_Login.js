be1b4760d0d1ed882dcbecefaa5a6a76751692bb
Tai <vothanhtai823@gmail.com>
2025-02-22 15:12:47 +0700

    push code saveCookies

--- Pages/TG_Login.js
+++ Pages/TG_Login.js
@@ -1,5 +1,6 @@
 import { config } from "../Utils/TG_Config";
-
+import fs from 'fs';
+import path from 'path';
 export class LoginAccount {
     constructor(page) {
         this.page = page;
@@ -13,5 +14,14 @@ export class LoginAccount {
         await page1.locator('#login-phone').fill('0329733010');
         await page1.getByRole('button', { name: 'Next' }).click();
     }
+    async saveCookies() {
+        const cookies = await this.page.context().cookies();
+        const dir = config.cookieDir;
+        const filePath = path.join(dir, config.cookiesFile);
 
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        fs.writeFileSync(filePath, JSON.stringify(cookies, null, 2));
+    }
 }
